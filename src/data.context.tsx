ad41df33927212436fc91a39c36c22d25e083ae5
Francisco Millone <franba2010@gmail.com>
2023-02-13 19:18:04 -0300

    added type checks for contexts

--- src/data.context.tsx
+++ src/data.context.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import data from "./data/data.json";
 import articles from "./data/articles.json";
+import type { ArticlesData, Data } from "./types";
 
 
-export const DataContext = React.createContext(data);
+export const DataContext = React.createContext<Data>(data);
 export const DataProvider = DataContext.Provider;
-export const ArticleContext = React.createContext(articles);
+export const ArticleContext = React.createContext<ArticlesData>(articles);
